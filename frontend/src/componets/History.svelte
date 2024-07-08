553c9fe31fe6aa554ad28400ea44ede28575b324
vitalspace <myglobalsounds@gmail.com>
2024-07-07 22:57:30 -0600

    History component

---
+++ frontend/src/componets/History.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { AngleLeftOutline } from "flowbite-svelte-icons";
+</script>
+
+<div class="fixed flex left-0 top-0 right-0 p-4">
+  <div class="w-full">
+    <button
+      class="grid place-items-center rounded-md bg-[#2997C7] p-1"
+      on:click={() => window.history.back()}
+    >
+      <AngleLeftOutline color="white" size="md" />
+    </button>
+  </div>
+</div>
