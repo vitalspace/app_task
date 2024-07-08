7eb188c07e6764e27a437c3747da55933f2f9c7b
vitalspace <myglobalsounds@gmail.com>
2024-07-07 22:57:50 -0600

    menu component

---
+++ frontend/src/componets/Menu.svelte
@@ -0,0 +1,62 @@
+<script>
+  import { Link } from "svelte-routing";
+</script>
+<div
+  class=" fixed bottom-5 flex place-items-center w-full justify-around py-2 bg-[#1A1A1A]"
+>
+  <Link to="/dashboard">
+    <svg
+      class="h-6 w-6 text-gray-800 dark:text-white"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  </Link>
+
+  <Link to="/createtask" class="rounded-full bg-[#2997C7] p-2">
+    <svg
+      class="h-6 w-6 text-gray-800 dark:text-white"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M5 12h14m-7 7V5"
+      />
+    </svg>
+  </Link>
+
+  <Link to="/profile">
+    <svg
+      class="h-6 w-6 text-gray-800 dark:text-white"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  </Link>
+</div>
