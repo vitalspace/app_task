f0867b95c76bb8a37cc6c668200f354a27b9b8f4
vitalspace <myglobalsounds@gmail.com>
2024-07-07 23:01:22 -0600

    auth store

---
+++ frontend/src/stores/auth.ts
@@ -0,0 +1,46 @@
+import { writable } from "svelte/store";
+
+function createAuthStore() {
+  const { subscribe, set } = writable(false);
+
+  return {
+    subscribe,
+    login: (jwt: string) => {
+      document.cookie = `auth=${jwt};path=/;max-age=7*86400;Secure;SameSite=Lax`;
+      set(true);
+    },
+    logout: () => {
+      document.cookie = `auth=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+      set(false);
+    },
+    initialize: () => {
+      const token = document.cookie.replace(
+        /(?:(?:^|.*;\s*)auth\s*\=\s*([^;]*).*$)|^.*$/,
+        "$1"
+      );
+      set(!!token);
+    },
+  };
+}
+
+export const auth = createAuthStore();
+
+// function createAuthStore() {
+//   const { subscribe, set } = writable(false);
+
+//   return {
+//     subscribe,
+//     login: (jwt: string) => {
+//       localStorage.setItem('token', jwt);
+//       set(true);
+//     },
+//     logout: () => {
+//       localStorage.removeItem('token');
+//       set(false);
+//     },
+//     initialize: () => {
+//       const token = localStorage.getItem('token');
+//       set(!!token);
+//     }
+//   };
+// }
