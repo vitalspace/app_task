e0030c3c531d8760cb8802a3f4b61dd816c901bc
vitalspace <myglobalsounds@gmail.com>
2024-07-07 22:58:31 -0600

    protected router

---
+++ frontend/src/componets/protectedRoute.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { navigate } from "svelte-routing";
+  import { auth } from "../stores/auth";
+  import { onMount } from "svelte";
+
+  export let component: any;
+
+  let isAuthenticated = false;
+
+  onMount(() => {
+    const unsubscribe = auth.subscribe((value) => {
+      isAuthenticated = value;
+      if (!isAuthenticated) {
+        navigate("/signin");
+      }
+    });
+
+    return unsubscribe;
+  });
+</script>
+
+{#if isAuthenticated}
+  <svelte:component this={component} />
+{/if}
