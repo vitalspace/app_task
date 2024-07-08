aaf0022debb227dacf7769b783723690210df7d6
vitalspace <myglobalsounds@gmail.com>
2024-07-07 22:55:36 -0600

    taskModel

---
+++ backend/src/models/task.model.ts
@@ -0,0 +1,45 @@
+import mongoose, { type Document } from "mongoose";
+
+// enum TaskType {
+//   Daily,
+//   Work,
+//   Personal,
+//   Leisure,
+//   Health,
+//   Relationship,
+//   Finance,
+// }
+
+interface ITask extends Document {
+  title: string;
+  description: string;
+  completed: boolean;
+  author: string;
+  type: string;
+}
+
+const taskSchema: mongoose.Schema<ITask> = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+});
+
+const Task = mongoose.model<ITask>("Task", taskSchema);
+export { Task };
